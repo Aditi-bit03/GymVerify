25071035d5753be5d8f29f92846095df911e7e72
Aditi <Aditipriya6202@gmail.com>
2025-05-23 18:42:12 +0530

    Initial commit for Gym Application

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        Designed And Developed By Aditi
+    </footer>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/WorkoutSessions.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const WorkoutSessions = () => {
+  return (
+    <section className="workout_session">
+      <div className="wrapper">
+        <h1>TOP WORKOUT SESSION</h1>
+        <p>
+        Top workout sessions combine strength, cardio, and flexibility exercises to improve overall fitness. They often include HIIT, circuit training, or weightlifting, designed for maximum efficiency and results. These sessions boost stamina, burn calories, and build muscle, making them ideal for all fitness levels and effective for achieving health goals.
+        </p>
+        <img src="/img5.jpg" alt="workout" />
+      </div>
+      <div className="wrapper">
+        <h1>FEATURED BOOTCAMPS</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+          impedit quasi sunt amet rerum accusamus odio eveniet unde?
+        </p>
+        <div className="bootcamps">
+          <div>
+            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+              saepe repellendus nemo sit facere ipsam!
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+              saepe repellendus nemo sit facere ipsam!
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+              saepe repellendus nemo sit facere ipsam!
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+              saepe repellendus nemo sit facere ipsam!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WorkoutSessions;
